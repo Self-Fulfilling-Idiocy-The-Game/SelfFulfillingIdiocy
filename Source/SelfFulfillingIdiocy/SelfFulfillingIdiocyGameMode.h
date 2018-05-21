78307bd9b7dc622ce91611850d8d52cbdb1e8bdb
Black-Photon <jkeane2002@gmail.com>
2018-05-21 16:18:14 +0100

    Initial Commit

---
+++ Source/SelfFulfillingIdiocy/SelfFulfillingIdiocyGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SelfFulfillingIdiocyGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASelfFulfillingIdiocyGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASelfFulfillingIdiocyGameMode();
+};
+
+
+
